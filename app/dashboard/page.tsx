dbb00ce36e1cc487b9b4d984504e7076bfa14d54
Nahuel Maika <nahuelmaika@gmail.com>
2026-04-21 19:12:57 -0300

    Add password recovery redirect and signout functionality
    - Implemented a redirect for password recovery requests to the update-password page.
    - Added a new signout route that handles user signout and redirects to the login page.
    - Updated the dashboard page to include a signout button for user convenience.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <div>DashboardPage
+      <div>
+        <form action="/api/auth/signout" method="post">
+          <button className="button block" type="submit">
+            Sign out
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
